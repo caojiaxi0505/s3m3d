1d52e03291cccc14bd8aed15b6b38b5dc20c51cc
s3m3d <s3m3d@gmail.com>
2025-10-05 13:35:02 +0800

    res101融合模型训练使用spconv2转化后的预训练权重

--- plugin/futr3d/configs/lidar_cam/lidar_0075v_cam_res101.py
+++ plugin/futr3d/configs/lidar_cam/lidar_0075v_cam_res101.py
@@ -386,4 +386,4 @@ lr_config = dict(
     min_lr_ratio=1e-3)
 checkpoint_config = dict(interval=1, max_keep_ckpts=1)
 
-load_from = 'checkpoint/lidar_0075_cam_res101.pth'
+load_from = 'checkpoint/lidar_0075_cam_res101_spconv2.pth'
